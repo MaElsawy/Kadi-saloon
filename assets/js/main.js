ba3e38bf0a4b8b296dc8fb6da1dc9d1c2abcb8bf
Your Name <your.email@example.com>
2024-11-23 03:26:17 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,166 @@
+function openNav() {
+    
+    document.getElementById("sidebar").classList.remove("-translate-x-full");
+
+    document.getElementById("sidebar").classList.add("translate-x-0");
+
+}
+
+function closeNav() {
+
+    document.getElementById("sidebar").classList.remove("translate-x-0");
+
+    document.getElementById("sidebar").classList.add("-translate-x-full");
+}
+
+let table = document.getElementById("table");
+
+let report = document.getElementById("report");
+
+let invoice = document.getElementById("invoice");
+
+
+let tablep = document.getElementById("Table-p");
+
+let reportp = document.getElementById("Report-p");
+
+let invoicep = document.getElementById("Invoice-p");
+
+
+let booking1 = document.getElementById("Booking_1");
+
+let booking2 = document.getElementById("Booking_2");
+
+let booking3 = document.getElementById("Booking_3");
+
+
+let customer = document.getElementById("customer");
+
+let partner = document.getElementById("partner");
+
+let Customerbtn = document.getElementById("Customer-btn");
+
+let Partnerbtn = document.getElementById("Partner-btn");
+
+
+function showTable() {
+
+    table.classList.remove("hidden");
+
+    report.classList.add("hidden");
+
+    invoice.classList.add("hidden");
+
+
+    tablep.classList.add("text-third");
+
+    reportp.classList.remove("text-third");
+
+    invoicep.classList.remove("text-third");
+
+
+    booking1.classList.remove("hidden");
+
+    booking2.classList.add("hidden");
+
+    booking3.classList.add("hidden");
+}
+function showReport() {
+    table.classList.add("hidden");
+
+    report.classList.remove("hidden");
+
+    report.classList.add("flex");
+
+    invoice.classList.add("hidden");
+
+
+    tablep.classList.remove("text-third");
+
+    tablep.classList.add("text-tenth");
+
+    reportp.classList.add("text-third");
+
+    invoicep.classList.remove("text-third");
+
+
+    booking1.classList.add("hidden");
+
+    booking2.classList.remove("hidden");
+
+    booking3.classList.add("hidden");
+}
+function showInvoice() {
+    table.classList.add("hidden");
+
+    report.classList.add("hidden");
+
+    invoice.classList.add("flex");
+
+    invoice.classList.remove("hidden");
+
+    tablep.classList.remove("text-third");
+
+    tablep.classList.add("text-tenth");
+
+    reportp.classList.remove("text-third");
+
+    invoicep.classList.add("text-third");
+
+
+    booking1.classList.add("hidden");
+
+    booking2.classList.add("hidden");
+
+    booking3.classList.remove("hidden");
+}
+function showCustomer() {
+
+    customer.classList.remove("hidden");
+
+    partner.classList.add("hidden");
+
+    Partnerbtn.classList.remove("text-primary");
+
+    Partnerbtn.classList.remove("font-extrabold");
+
+    Customerbtn.classList.add("text-primary");
+
+    Customerbtn.classList.add("font-extrabold");
+
+    Customerbtn.classList.remove("text-tenth");
+
+    Customerbtn.classList.remove("font-medium");
+
+    Partnerbtn.classList.add("text-tenth");
+
+    Partnerbtn.classList.remove("bg-white");
+
+    Customerbtn.classList.add("bg-white");
+}
+function showPartner() {
+
+    customer.classList.add("hidden");
+
+    partner.classList.remove("hidden");
+
+    Partnerbtn.classList.add("text-primary");
+
+    Partnerbtn.classList.add("font-extrabold");
+
+    Partnerbtn.classList.remove("text-tenth");
+
+    Partnerbtn.classList.remove("font-medium");
+
+    Customerbtn.classList.remove("text-primary");
+
+    Customerbtn.classList.remove("font-extrabold");
+
+    Customerbtn.classList.add("text-tenth");
+
+    Customerbtn.classList.add("font-medium");
+
+    Customerbtn.classList.remove("bg-white");
+
+    Partnerbtn.classList.add("bg-white");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html","./assets/**/*.{html,js} ./index.html","./assets/**/*.js","./assets/**/*.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+      colors: {
+        primary: "#643E96",
+        secondary: "#F1ECFB",
+        third:"#374151",
+        forth:"#C891C3",
+        fifth:"#3C3F4F",
+        sixth:"#C691C117",
+        seventh:"#C691C199",
+        eighth:"#643E981F",
+        ninth:"#C691C10A",
+        tenth:"#6B7280",
+        eleventh:"#111827",
+        twelfth:"#F6F8FC",
+        twelveth:"#EDF1F9",
+      },
+      fontFamily: {
+        primary: ["Noto Sans Arabic"],
+      },
+
+    },
+  },
+  plugins: [],
+}
+
