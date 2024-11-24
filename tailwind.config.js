dfc74d7201fc2cf92c397e29ff558b8e5290edff
Your Name <your.email@example.com>
2024-11-24 10:33:45 +0200

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,15 @@ module.exports = {
       fontFamily: {
         primary: ["Noto Sans Arabic"],
       },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(-20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1.5s ease-out',
+      },
 
     },
   },
